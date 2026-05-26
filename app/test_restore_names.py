af8c15e51a8d441bec6a55b6239ab8e07933a8d7
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-05-26 18:04:22 +0300

    Fixed flake8

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -2,20 +2,44 @@ from typing import Any
 import pytest
 from app.restore_names import restore_names
 
+
 @pytest.mark.parametrize(
     "init_users, expected_users",
     [
         (
             [
-                {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
-                {"last_name": "Holy", "full_name": "Jack Holy"},
-                {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                },
+                {
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                },
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }
             ],
 
             [
-                {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-                {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-                {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                },
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                },
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                }
             ]
         ),
     ]
